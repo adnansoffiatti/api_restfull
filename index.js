f896074caa5b24ef0007a87baea90e365280ae18
adnansoffiatti <adnan.soffiatti@gmail.com>
2022-08-12 13:11:42 -0300

    Configurando o banco de dados MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT;
 
@@ -12,4 +13,17 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 })
 
-app.listen(port, () => console.log('Servidor rodando...'));
+
+// conectando ao banco e configurando rota
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apirestfull.juxnute.mongodb.net/?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log('Conectamos ao MongoDB');
+    app.listen(port, () => console.log('Servidor rodando...'));
+  })
+  .catch((err) => console.log(err));
+
+
+
